3d58d87766cc9048f65d3cf4045023964568a900
dguerramoruno <dguerramoruno@gmail.com>
2024-03-20 19:27:03 +0100

    Some changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const http  = require('node:http')
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/userRoutes')
+const reservationRoutes = require('./routes/reservationRoutes')
+
 
 const desiredPort = porcess.env.PORT ?? 1234
 
